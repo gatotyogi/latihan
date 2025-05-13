04ab767a5812ea826a4430183704da420de9d982
ogi <gatotyogiwilujeng@gmail.com>
2025-05-13 22:34:51 +0700

    Update

--- constanta/index.php
+++ constanta/index.php
@@ -4,6 +4,26 @@
 // echo situs;
 
 //menggunakan define
-define("situs", "yogi.com");
-echo situs;
+// define("situs", "yogi.com");
+// echo situs;
+
+/* menampilkan variabel dan konstanta*/
+// $nama="saroh";
+// define("GAJI", 1000000);
+// echo $nama;
+// echo "<br>";
+// echo GAJI;
+$nama="saroh";
+$umur=23;
+$kerja="Guru Hononer";
+define("GAJI", 1000000);
+// echo "Nama Saya $nama umur $umur pekerjaan $kerja dengan gaji /bulan Rp. ". GAJI;
+echo "Nama: $nama";
+echo "<br>";
+echo "Umur: $umur";
+echo "<br>";
+echo "Pekerjaan: $kerja";
+echo "<br>";
+echo "Gaji: Rp. ".GAJI;
+
 ?>
