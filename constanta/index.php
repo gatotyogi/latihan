7538b021dcaafe5b9f15ea745373021cb8d430b2
ogi <gatotyogiwilujeng@gmail.com>
2025-05-13 23:10:26 +0700

    update

--- constanta/index.php
+++ constanta/index.php
@@ -8,16 +8,11 @@
 // echo situs;
 
 /* menampilkan variabel dan konstanta*/
-// $nama="saroh";
-// define("GAJI", 1000000);
-// echo $nama;
-// echo "<br>";
-// echo GAJI;
 $nama="saroh";
 $umur=23;
 $kerja="Guru Honorer";
 define("GAJI", 1000000);
-// echo "Nama Saya $nama umur $umur pekerjaan $kerja dengan gaji /bulan Rp. ". GAJI;
+
 echo "Nama: $nama";
 echo "<br>";
 echo "Umur: $umur";
@@ -28,5 +23,6 @@ echo "Gaji: Rp. ".GAJI;
 //satu baris
 echo "<br><br>";
 echo "Nama saya $nama, umur $umur tahun, pekerjaan $kerja dengan gaji Rp. ".GAJI;
+
 
 ?>
